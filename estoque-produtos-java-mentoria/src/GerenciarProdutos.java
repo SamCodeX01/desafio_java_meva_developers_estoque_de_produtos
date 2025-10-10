497df558b9b580a73f9bc014172c46ad945f6f47
SamCodeX01 <txtstring@gmail.com>
2025-10-10 16:44:36 -0300

    Atualizando

--- estoque-produtos-java-mentoria/src/GerenciarProdutos.java
+++ estoque-produtos-java-mentoria/src/GerenciarProdutos.java
@@ -21,3 +21,73 @@ public class GerenciarProdutos {
         }
 
 }
+
+
+/*
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Produtos> produtosss = new ArrayList<>();
+        produtosss.add(new Instrumentos("Violino", 6));
+        produtosss.add(new Instrumentos("Flauta", 2));
+        produtosss.add(new Comida("Pudim", 2));
+
+        for(Produtos p: produtosss){
+//            System.out.println(p.nome);
+//            System.out.println(p.qtd);
+            p.tocar();
+        }
+
+    }
+}
+
+
+public interface Produtos {
+
+    public void tocar();
+}
+
+
+
+public class Produtosss implements Produtos{
+    public String nome;
+    public int qtd;
+
+    public Produtosss(String nome, int qtd) {
+        this.nome = nome;
+        this.qtd = qtd;
+    }
+
+    public void tocar(){
+        System.out.println("Tocando instrumento!");
+    }
+
+
+
+
+public class Instrumentos implements Produtos{
+    public String nome;
+    public int qtd;
+
+    public Instrumentos(String nome, int qtd) {
+        this.nome = nome;
+        this.qtd = qtd;
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("TOCANDO INSTRUMENTOS!");
+    }
+}
+
+
+}
+
+
+
+
+
+
+
+*/
